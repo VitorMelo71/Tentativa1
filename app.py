07e426b11919f845a57228b1cbfbd4acbc2cb97e
VitorMelo71 <vitor.melo@itec.ufpa.br>
2024-09-25 17:40:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json
 import tempfile
-import time
-import folium
-from streamlit_folium import st_folium
 
 # Carregar as credenciais do Firebase a partir do Secrets do Streamlit
 firebase_credentials = st.secrets["firebase_credentials"]
 
+# Verificar se as credenciais são um dicionário e convertê-las, se necessário
+if isinstance(firebase_credentials, dict):
+    credentials_dict = firebase_credentials
+else:
+    credentials_dict = dict(firebase_credentials)
+
 # Criar um arquivo temporário para armazenar as credenciais do Firebase
 with tempfile.NamedTemporaryFile(mode='w+', suffix='.json', delete=False) as temp_file:
-    json.dump(firebase_credentials, temp_file)
+    json.dump(credentials_dict, temp_file)
     temp_file.flush()
 
     # Inicializar Firebase com o caminho do arquivo temporário de credenciais
@@ -24,35 +27,3 @@ with tempfile.NamedTemporaryFile(mode='w+', suffix='.json', delete=False) as tem
 db = firestore.client()
 
 st.write("Firebase conectado com sucesso!")
-
-# Função para buscar a última localização do Firestore
-def buscar_localizacao():
-    doc_ref = db.collection(u'CoordenadasGPS').document(u'veiculo')
-    doc = doc_ref.get()
-    if doc.exists:
-        data = doc.to_dict()
-        latitude = data.get('latitude')
-        longitude = data.get('longitude')
-        status = data.get('status')
-        return float(latitude), float(longitude), status
-    else:
-        return None, None, None
-
-# Função para exibir o mapa no Streamlit
-def exibir_mapa(latitude, longitude):
-    mapa = folium.Map(location=[latitude, longitude], zoom_start=15)
-    folium.Marker([latitude, longitude], tooltip="Ônibus").add_to(mapa)
-    st_folium(mapa, width=725)
-
-st.title('Rastreamento de Ônibus em Tempo Real')
-
-# Atualizar localização a cada 10 segundos
-while True:
-    latitude, longitude, status = buscar_localizacao()
-    if latitude is not None and longitude is not None:
-        st.write(f"Localização atual: Latitude {latitude}, Longitude {longitude}, Status: {status}")
-        exibir_mapa(latitude, longitude)
-    else:
-        st.write("Aguardando atualização de localização...")
-
-    time.sleep(10)  # Atualizar a cada 10 segundos
