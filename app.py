04c469cab28224df25744f45b371e13ab3c828ef
VitorMelo71 <vitor.melo@itec.ufpa.br>
2024-09-24 22:37:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json  # Adicionar o módulo JSON para converter o conteúdo
 
 # Carregar as credenciais do Firebase a partir do Secrets do Streamlit
 firebase_credentials = st.secrets["firebase_credentials"]
 
-# Inicializar Firebase com as credenciais do Secret Manager
-cred = credentials.Certificate(firebase_credentials)
+# Converter o conteúdo JSON das credenciais para um dicionário
+firebase_credentials_dict = json.loads(json.dumps(firebase_credentials))
+
+# Inicializar Firebase com o dicionário de credenciais
+cred = credentials.Certificate(firebase_credentials_dict)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -19,3 +23,5 @@ def enviar_para_firebase(latitude, longitude, status):
         u'status': status
     })
     print(f"Dados enviados ao Firebase: Latitude {latitude}, Longitude {longitude}, Status {status}")
+
+# Sua lógica principal continua abaixo
