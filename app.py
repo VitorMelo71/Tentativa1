5aeeda85d38e5346b75a7cf0b1d473a4676370ef
VitorMelo71 <vitor.melo@itec.ufpa.br>
2024-09-24 22:42:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import json  # Adicionar o módulo JSON para converter o conteúdo
 
-# Carregar as credenciais do Firebase a partir do Secrets do Streamlit
+# Carregar as credenciais do Firebase diretamente das secrets do Streamlit
 firebase_credentials = st.secrets["firebase_credentials"]
 
-# Converter o conteúdo JSON das credenciais para um dicionário
-firebase_credentials_dict = json.loads(json.dumps(firebase_credentials))
-
 # Inicializar Firebase com o dicionário de credenciais
-cred = credentials.Certificate(firebase_credentials_dict)
+cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
