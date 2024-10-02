9b63b365ae7ac3d348af352e20eed4ac8c643a0e
VitorMelo71 <vitor.melo@itec.ufpa.br>
2024-10-01 22:39:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,20 @@
-import streamlit as st
 import firebase_admin
-from firebase_admin import credentials, firestore
-import json
-import tempfile
+from firebase_admin import credentials
+import os
 
-# Interface para upload do arquivo JSON no Streamlit
-uploaded_file = st.file_uploader("Upload seu arquivo JSON do Firebase", type="json")
+firebase_credentials = {
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+}
 
-if uploaded_file is not None:
-    # Salvar o arquivo temporariamente
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        json_file_path = tmp_file.name
-
-    # Inicializar Firebase com o arquivo JSON carregado
-    if not firebase_admin._apps:
-        cred = credentials.Certificate(json_file_path)
-        firebase_admin.initialize_app(cred)
-
-    # Inicializar o Firestore
-    db = firestore.client()
-
-    # Exemplo de uma consulta ao Firestore
-    def buscar_localizacao():
-        doc_ref = db.collection(u'CoordenadasGPS').document(u'veiculo')
-        doc = doc_ref.get()
-        if doc.exists:
-            data = doc.to_dict()
-            latitude = data.get('latitude')
-            longitude = data.get('longitude')
-            status = data.get('status')
-            return float(latitude), float(longitude), status
-        else:
-            return None, None, None
-
-    # Use a função para buscar e exibir a localização
-    latitude, longitude, status = buscar_localizacao()
-    if latitude is not None and longitude is not None:
-        st.write(f"Localização: {latitude}, {longitude} - Status: {status}")
-    else:
-        st.write("Aguardando atualização de localização...")
+if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_credentials)
+    firebase_admin.initialize_app(cred)
