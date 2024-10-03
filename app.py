837f3084db970d24fc1c88ddbc6f7462c9ffe64b
VitorMelo71 <vitor.melo@itec.ufpa.br>
2024-10-03 08:51:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import streamlit.components.v1 as components
 import streamlit as st
+import streamlit.components.v1 as components
 import requests
 import time
 
@@ -29,86 +29,69 @@ def get_tracking_data():
             })
     return records
 
-# Configuração da página
+# Configuração da página para celular
 st.set_page_config(page_title="CEAMAZON GPS - Rastreamento", layout="centered")
-
-# Modo dark ao fundo do site
-st.markdown("""
-    <style>
-    body {
-        background-color: #121212;
-    }
-    .stText, .stMarkdown, .stButton > button {
-        color: white;
-    }
-    </style>
-""", unsafe_allow_html=True)
 
 st.title("CEAMAZON GPS - Rastreamento")
 
-# Função para renderizar o mapa com JavaScript e o marcador
+# Inicializa o mapa do Google Maps apenas uma vez
 def render_map(lat, lon):
-    map_code = f"""
-    <html>
-      <head>
-        <script src="https://maps.googleapis.com/maps/api/js?key={GOOGLE_MAPS_API_KEY}"></script>
+    components.html(f"""
+        <html>
+          <head>
+            <script src="https://maps.googleapis.com/maps/api/js?key={GOOGLE_MAPS_API_KEY}"></script>
+            <script>
+              var marker;
+              var map;
+
+              function initMap() {{
+                var mapOptions = {{
+                  center: new google.maps.LatLng({lat}, {lon}),
+                  zoom: 15,
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+                }};
+                map = new google.maps.Map(document.getElementById("map"), mapOptions);
+                marker = new google.maps.Marker({{
+                  position: new google.maps.LatLng({lat}, {lon}),
+                  map: map,
+                  title: "Localização do Veículo"
+                }});
+              }}
+
+              function updateMarker(lat, lon) {{
+                var newPosition = new google.maps.LatLng(lat, lon);
+                marker.setPosition(newPosition);
+                map.setCenter(newPosition);
+              }}
+
+              window.updateMarker = updateMarker;  // Define a função global para poder ser chamada externamente
+            </script>
+          </head>
+          <body onload="initMap()">
+            <div id="map" style="width: 100%; height: 500px;"></div>
+          </body>
+        </html>
+    """, height=500)
+
+# Atualiza a posição do veículo sem recarregar o mapa inteiro
+def update_map(lat, lon):
+    components.html(f"""
         <script>
-          var marker;
-          var map;
-
-          function initMap() {{
-            var mapOptions = {{
-              center: new google.maps.LatLng({lat}, {lon}),
-              zoom: 15,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            }};
-            map = new google.maps.Map(document.getElementById("map"), mapOptions);
-            marker = new google.maps.Marker({{
-              position: new google.maps.LatLng({lat}, {lon}),
-              map: map,
-              title: "Localização do Veículo"
-            }});
-            window.map = map;
-            window.marker = marker;
-          }}
-
-          function updateMarker(lat, lon) {{
-            if (window.marker) {{
-              var newPosition = new google.maps.LatLng(lat, lon);
-              window.marker.setPosition(newPosition);
-              window.map.setCenter(newPosition);
-            }}
-          }}
-
-          window.initMap = initMap;
-          window.updateMarker = updateMarker;
+            window.updateMarker({lat}, {lon});
         </script>
-      </head>
-      <body onload="initMap()" style="background-color: #121212;">
-        <div id="map" style="width: 100%; height: 500px;"></div>
-      </body>
-    </html>
-    """
-    components.html(map_code, height=500)
+    """, height=0)
 
-# Carrega o mapa apenas uma vez
+# Pega a posição inicial para renderizar o mapa
 data = get_tracking_data()
+
 if data:
     latest_data = data[0]
     render_map(latest_data['latitude'], latest_data['longitude'])
 
-# Configuração para atualizar o marcador a cada 10 segundos
-update_interval = 3  # segundos
-
-# Loop de atualização do marcador
+# Atualiza a posição do veículo a cada 10 segundos
 while True:
     data = get_tracking_data()
     if data:
         latest_data = data[0]
-        update_code = f"""
-        <script>
-            updateMarker({latest_data['latitude']}, {latest_data['longitude']});
-        </script>
-        """
-        components.html(update_code, height=0)  # Apenas atualiza o marcador sem renderizar um novo mapa
-    time.sleep(update_interval)
+        update_map(latest_data['latitude'], latest_data['longitude'])
+    time.sleep(10)
